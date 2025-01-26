d9e9c92a54363a90cbd1e26cdc22acbec7be38bc
gmurad97 <murad.dev@bk.ru>
2025-01-26 21:32:47 +0400

    rebuild

--- apps/pages/admin.py
+++ apps/pages/admin.py
@@ -1,15 +1,3 @@
 from django.contrib import admin
-from .models import News,Comments
 
 # Register your models here.
-
-@admin.register(News)
-class NewsAdmin(admin.ModelAdmin):
-    list_display = ("id","title","description")
-    list_filter = ("id","title")
-    # list_editable = ("title","description")
-
-@admin.register(Comments)
-class CommentsAdmin(admin.ModelAdmin):
-    list_display = ("id","first_name","last_name","comment_text","news")
-    list_filter = ("id","first_name","last_name","comment_text")
--- apps/pages/models.py
+++ apps/pages/models.py
@@ -1,25 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class News(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    status = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return str(self.title)
-
-class Comments(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    comment_text = models.TextField()
-    news = models.ForeignKey(News, on_delete=models.CASCADE)
-    status = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return str(self.first_name)
